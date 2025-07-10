b5712a69123e7a481008e80a3eb483113c1f8e9a
1alvaropaiva <alvaropaiva.dev@gmail.com>
2025-07-10 18:27:37 -0300

    Initial commit

---
+++ APIone/src/main/java/com/alvaropaiva/APIone/ApIoneApplication.java
@@ -0,0 +1,13 @@
+package com.alvaropaiva.APIone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIoneApplication.class, args);
+	}
+
+}
