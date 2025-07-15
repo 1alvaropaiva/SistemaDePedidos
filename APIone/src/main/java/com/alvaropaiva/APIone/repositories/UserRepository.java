cc66715e4d12b1023c07e9ff25be55118efb5afd
1alvaropaiva <alvaropaiva.dev@gmail.com>
2025-07-15 18:33:56 -0300

    Service layer, component registration

--- APIone/src/main/java/com/alvaropaiva/APIone/repositories/UserRepository.java
+++ APIone/src/main/java/com/alvaropaiva/APIone/repositories/UserRepository.java
@@ -3,7 +3,5 @@ package com.alvaropaiva.APIone.repositories;
 import com.alvaropaiva.APIone.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
-
-}
+//@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
