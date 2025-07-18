1fce1a0e7c5c4f8d05f0ab15dda45ba1de26a020
1alvaropaiva <alvaropaiva.dev@gmail.com>
2025-07-18 10:39:58 -0300

    Category

---
+++ APIone/src/main/java/com/alvaropaiva/APIone/repositories/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.alvaropaiva.APIone.repositories;
+
+import com.alvaropaiva.APIone.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {}
